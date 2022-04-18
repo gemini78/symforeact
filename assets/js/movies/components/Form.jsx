bb3d35e1bee69a05c059447e2c376e842fd25789
stebou mendy <stebou8mendy@gmail.com>
2022-04-18 18:54:11 +0200

    Implement search

--- assets/js/movies/components/Form.jsx
+++ assets/js/movies/components/Form.jsx
@@ -5,18 +5,18 @@ import Card from './Card';
 const Form = () => {
 
   const [moviesData, setMoviesData] = useState([])
-
+  const [search, setSearch] = useState("code")
   useEffect(() => {
-    axios.get('https://api.themoviedb.org/3/search/movie?api_key=5b42eb8b4c6c0c09ce850bfafd6fbe55&language=fr-FR&query=avenger').then(res => {
+    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=5b42eb8b4c6c0c09ce850bfafd6fbe55&language=fr-FR&query=${search}`).then(res => {
       setMoviesData(res.data.results);
     });
-  }, []);
+  }, [search]);
 
   return (
     <div className='form-component'>
       <div className='form-container'>
         <form action="">
-          <input type="text" placeholder='Entrer le titre du film' id="search-input" />
+          <input type="text" placeholder='Entrer le titre du film' id="search-input" onChange={(e) => setSearch(e.target.value)} />
           <input type="submit" value="Rechercher" />
         </form>
         <div className='btn-sort-container'>
