69d8cfce232ef73b6120824036ed18145e965246
stebou mendy <stebou8mendy@gmail.com>
2022-04-18 18:46:51 +0200

    Add card's component

--- assets/js/movies/components/Form.jsx
+++ assets/js/movies/components/Form.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import Card from './Card';
 
 const Form = () => {
+
+  const [moviesData, setMoviesData] = useState([])
+
+  useEffect(() => {
+    axios.get('https://api.themoviedb.org/3/search/movie?api_key=5b42eb8b4c6c0c09ce850bfafd6fbe55&language=fr-FR&query=avenger').then(res => {
+      setMoviesData(res.data.results);
+    });
+  }, []);
+
   return (
     <div className='form-component'>
       <div className='form-container'>
@@ -13,7 +24,11 @@ const Form = () => {
           <div className="btn-sort" id="badToGood">Flop<span>&#8594;</span></div>
         </div>
       </div>
-      <div className='result'></div>
+      <div className='result'>
+        {moviesData.slice(0, 12).map((movie) => (
+          <Card key={movie.id} movie={movie} />
+        ))}
+      </div>
     </div>
   )
 }
--- assets/js/movies/pages/Home.jsx
+++ assets/js/movies/pages/Home.jsx
@@ -4,7 +4,7 @@ import Header from '../components/Header';
 
 const Home = () => {
   return (
-    <div>
+    <div className='home-page'>
       <Header />
       <Form />
     </div>
